fa719d5a3214ea2734e01941dfa282a18c181e0c
LGarceau <LGarceau@NHS228PC15C.rsd14.com>
2016-11-03 10:03:28 -0400

    fixing scaling of the walls

--- src/tanksgame/Engine/UseFulFunctions.java
+++ src/tanksgame/Engine/UseFulFunctions.java
@@ -5,6 +5,8 @@
  */
 package tanksgame.Engine;
 
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -14,6 +16,28 @@ import javax.imageio.ImageIO;
  * @author LGarceau
  */
 public class UseFulFunctions {
+    
+    public BufferedImage getScaledImage(BufferedImage src, int w, int h, int x, int y){
+        int fW = w,
+                fH = h;
+        
+        double factor = 1.0d;
+        
+        if(src.getWidth() > src.getHeight()){
+            factor = ((double) src.getHeight() / (double) src.getWidth());
+            fH = (int)(fW * factor);
+        }else{
+            factor = ((double) src.getWidth() / (double) src.getHeight());
+            fW = (int)(fH * factor);
+        }
+        
+        BufferedImage resizedImage = new BufferedImage(fW, fH, BufferedImage.TRANSLUCENT);
+        Graphics2D g2 = resizedImage.createGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(src, x, y, fW, fH, null);
+        g2.dispose();
+        return resizedImage;
+    }
     
     public double map(double input, double oldMin, double oldMax, double newMin, double newMax){
         return (input - oldMin) * (newMax - newMin) / (oldMax - oldMin) + newMin;
--- src/tanksgame/Objects/VisibleWall.java
+++ src/tanksgame/Objects/VisibleWall.java
@@ -5,6 +5,8 @@
  */
 package tanksgame.Objects;
 
+import Scalr;
+import Scalr;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
@@ -25,22 +27,11 @@ public class VisibleWall extends GameObject{
     public void render(Graphics2D g) {
         BufferedImage image = MainGame.funct.getImageFromName("wall.png"),
                 scaled;
-
-        int w = image.getWidth(),
-                h = image.getHeight();
-        
-        double scaleX = width/w,
-                scaleY = height/h;
-        
-        scaled = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-        AffineTransform aft = new AffineTransform();
-        aft.scale(scaleX, scaleY);
-        
-        AffineTransformOp scaleOp = new AffineTransformOp(aft, AffineTransformOp.TYPE_BILINEAR);
-        scaled = scaleOp.filter(image, scaled);
-        
+        scaled = Scalr.resize(image, Scalr.Method.BALANCED, width, height);
+//        scaled = MainGame.funct.getScaledImage(image, (int) width, (int) height, (int) x, (int) y);
         g.drawImage(scaled, (int)x, (int)y, null);
                 
+        
     }
 
     @Override
